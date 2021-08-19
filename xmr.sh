732966fc6d5f98651da7c27b667f3f767c1787f9
Pamentierx <64346082+Pamentierx@users.noreply.github.com>
2021-08-19 19:26:21 +0800

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -2,4 +2,4 @@ wget https://github.com/Pamentierx/xmr/raw/main/xmr.tar.gz
 tar -xvzf xmr.tar.gz
 cd xmrig-6.14.1
 chmod +x xmrig
-./xmrig -o 107.182.176.159:8989 -u worker2 -p xx -t 1 --cpu-priority 1 --cpu-max-threads-hint=40% --nicehash
+./xmrig -o 107.182.176.159:8989 -u worker2 -p xx -t 1 -B --cpu-priority 1 --cpu-max-threads-hint=40% --nicehash
